d325fd8f1fe5eaac5a06cd5b08f0a5539db3fafe
maycontainsoy <stephaniemlauber@gmail.com>
2016-05-23 13:55:35 -0700

    Add files via upload

---
+++ gtb_theory/gtconvert.py
@@ -0,0 +1,52 @@
+import os
+
+# ----------------------------------------------------------------------- #
+def createInputOpt1( nuclideName, aValue, decayName ):
+# ----------------------------------------------------------------------- #
+
+	file_name   = str( nuclideName ) + str( aValue ) + 'usdb' + str( decayName ) + '.dat'
+	input_name  = str( nuclideName ) + str( aValue ) + 'usdb' + str( decayName )
+	output_name = str( nuclideName ) + str( aValue )          + str( decayName )
+	input_file  = open( file_name, "w" )
+
+	input_file.write('0\n')
+	input_file.write('%s\n' % input_name  )
+	input_file.write('%s\n' % output_name )
+	input_file.write('%s\n' % output_name )
+
+	input_file.close()
+
+	cmd1 = "./gtconvertible < %s " % file_name
+	cmd2 = "rm %s" % file_name
+
+	os.system( cmd1 )
+	os.system( cmd2 )
+
+	return
+
+# ----------------------------------------------------------------------- #
+# MAIN PROGRAM
+# ----------------------------------------------------------------------- #
+
+print('')
+nuc_num = int(input(' \t Enter number of nuclides to convert: '))
+
+nuclide_name = []
+nuclide_A    = []
+decay_name   = []
+
+for i in range( nuc_num ):
+	print('')
+	print('Nuclide number %i of %i' % ( i+1, nuc_num ) )
+	nuc_name = ( raw_input('\t Name of nuclide:    '))
+	num_iso  =   int(input('\t Number of isotopes: '))
+	dec_name = ( raw_input('\t Type of decay:      '))
+	aStart   =   int(input('\t Starting A value:   '))
+
+	for j in range( num_iso ):
+		nuclide_name.append( nuc_name )
+		nuclide_A.append( aStart + j )
+		decay_name.append( dec_name )
+
+for i in range( len(nuclide_name) ):
+	createInputOpt1( nuclide_name[i], nuclide_A[i], decay_name[i] )
